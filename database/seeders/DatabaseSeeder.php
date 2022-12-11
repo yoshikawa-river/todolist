af026156b61e4e515e6d0c0666053188f23b0d1e
yoshikawa-river <yuttta0411@gmail.com>
2022-12-11 21:47:26 +0900

    外部キー制約の解除と、taskSeederの実行を追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,8 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
         $this->call([
             UserSeeder::class,
+            TaskSeeder::class
         ]);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
     }
 }
