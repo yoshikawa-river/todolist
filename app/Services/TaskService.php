064b9a801b54d50e8e8cf71e6c4b7914e26905b5
yoshikawa-river <yuttta0411@gmail.com>
2022-12-18 17:09:52 +0900

    タグを削除した時のバグを修正

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -26,6 +26,15 @@ class TaskService
         DB::transaction(function () use($params, $task) {
             $task->fill($params->toArray())->save();
 
+            $params_tags = collect($params->get('tags'))->pluck('id');
+
+            foreach ($task->tags as $value) {
+                if (!$params_tags->contains($value->id)) {
+                    $tag = Tag::find($value->id);
+                    $tag->delete();
+                }
+            }
+
             foreach ($params->get('tags') as $value) {
                 if (is_null($value['id'])) {
                     unset($value['id']);
