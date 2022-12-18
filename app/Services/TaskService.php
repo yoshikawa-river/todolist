73957348a55e4d174f3fe2e616653d8e47a952e1
yoshikawa-river <yuttta0411@gmail.com>
2022-12-18 16:24:11 +0900

    タスクとタグの更新処理を作成

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\Tag;
 use App\Models\Task;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
@@ -25,8 +26,19 @@ class TaskService
         DB::transaction(function () use($params, $task) {
             $task->fill($params->toArray())->save();
 
-            $tags = $params->get('tags');
-            $task->tags()->fill($tags)->save();
+            $param_tags = $params->get('tags');
+            
+            foreach ($params->get('tags') as $value) {
+                if (is_null($value['id'])) {
+                    unset($value['id']);
+                    $task->tags()->create($value);
+                } else {
+                    $tag = Tag::find($value['id']);
+                    unset($value['id']);
+                    $tag->fill($value)->save();
+                }
+            }
+            
         });
     }
 }
